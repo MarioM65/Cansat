0896ad00b148ffe0ddace7c348e19989252ab37b
MarioM65 <mmystirio7@gmail.com>
2025-05-24 17:07:31 +0100

    alter

---
+++ Janela/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Janela
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClique_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Você clicou no botão!");
+        }
+    }
+}
